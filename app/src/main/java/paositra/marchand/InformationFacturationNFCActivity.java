12da40dfba9aac92d0cd5edf68c273685998178c
hary-aina <harynyainarajaonah01@gmail.com>
2023-06-15 10:43:19 +0300

    ajout InformationFacturationNFCActivity

---
+++ app/src/main/java/paositra/marchand/InformationFacturationNFCActivity.java
@@ -0,0 +1,14 @@
+package paositra.marchand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationFacturationNFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_facturation_nf);
+    }
+}
