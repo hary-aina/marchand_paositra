f96e7a10e2bd700564557c5d1b11b4fa0f0e5003
hary-aina <harynyainarajaonah01@gmail.com>
2023-06-15 21:00:54 +0300

    cardReader nfc

--- app/src/main/java/paositra/marchand/InfoFacturationNFCActivity.java
+++ app/src/main/java/paositra/marchand/InfoFacturationNFCActivity.java
@@ -3,9 +3,11 @@ package paositra.marchand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -27,6 +29,15 @@ public class InfoFacturationNFCActivity extends AppCompatActivity {
         TextView marchand = (TextView) findViewById(R.id.marchand);
         marchand.setText("AR "+preferences.getString("marchand", ""));
 
+        //start NFC card reader
+        Button launch_NFC_Reader = (Button)findViewById(R.id.launch_NFC_Reader);
+        launch_NFC_Reader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNfcCardReader(v);
+            }
+        });
+
         //finish
         ImageButton returnBtn = (ImageButton)findViewById(R.id.retour);
         returnBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +46,11 @@ public class InfoFacturationNFCActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    //start NFC card reader
+    public void startNfcCardReader(View v){
+        Intent nfcCardReaderActivity = new Intent(v.getContext().getApplicationContext(), NfcCardReaderActivity.class);
+        startActivity(nfcCardReaderActivity);
     }
 }
