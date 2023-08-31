473435fedbd650b4944f017566b6eb78f5ba5a94
hary-aina <harynyainarajaonah01@gmail.com>
2023-08-31 13:33:18 +0300

    gestion du systeme de paiement

--- app/src/main/java/paositra/marchand/clientApi/RetrofitClient.java
+++ app/src/main/java/paositra/marchand/clientApi/RetrofitClient.java
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.88.193:82/"; // Replace with your API base URL
+    private static final String BASE_URL = "http://api-phco-marchand.numherit-dev.com/"; // Replace with your API base URL
+    //private static final String BASE_URL = "http://192.168.88.193:82/"; // Replace with your API base URL
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
